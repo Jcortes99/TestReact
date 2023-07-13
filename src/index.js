19c0aa214a724d1eee5551678cded6ab2e86123f
jcortes-intecol <jcortes@intecol.com.co>
2023-07-13 11:28:24 -0500

    Update counter click

--- src/index.js
+++ src/index.js
@@ -5,19 +5,36 @@ import { useState } from 'react'
 const App = () => {
   const [ counter, setCounter ] = useState(0)
 
+  const increaseByOne = () => setCounter(counter+1)
+
+  const setZero = () => setCounter(0)
+
+  const increaseBy2 = () => setCounter(counter+2)
+
   return (
     <div>
-      <div>{counter}</div>
+      <Display counter={counter}/>
 
-      <button onClick={() => setCounter(counter + 1)}>
-        plus
-      </button>
-      <button onClick={() => setCounter(0)}> 
-        zero
-      </button>
+      <Button 
+      handleClick={increaseByOne}
+      text='+1'
+      />
+
+      <Button 
+      handleClick={increaseBy2}
+      text='+2'
+      />
+
+      <Button 
+      handleClick={setZero}
+      text='0'
+      />
     </div>
   )
 }
 
+const Display = ({ counter }) => <div>{counter}</div>
+
+const Button = ({ handleClick, text}) => <button onClick={handleClick}>{text}</button>
 
 ReactDOM.render(<App />, document.getElementById('root'))
